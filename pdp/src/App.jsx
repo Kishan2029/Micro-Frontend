80d4e5c34db8c9c2e692f61b395855cade5cf355
= <=>
2024-02-13 14:01:34 +0530

    header and footer added in home app; pdp micro app created

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
-import Header from "./Header";
-import Footer from "./Footer";
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <Header />
-    <div className="my-10">Home page content</div>
-    <Footer />
+    <div>Name: pdp</div>
+    <div>Framework: react</div>
+    <div>Language: JavaScript</div>
+    <div>CSS: Tailwind</div>
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
